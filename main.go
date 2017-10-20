bca41fa43074ae5f83ce39b9755cc0c9d7869b49
Leonardo Souza <leogsouza@gmail.com>
2017-10-20 13:16:17 +0000

    Add chat template

--- main.go
+++ main.go
@@ -4,24 +4,31 @@ import (
 	"github.com/leogsouza/go-chat/defaultport"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
 	port := utils.DefaultPort()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                </head>
-                <body>
-                    Let's chat!
-                </body>
-            </html>
-        `))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
